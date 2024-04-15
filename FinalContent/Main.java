cdea99658d0c17c3d795ddb3faae26d70b3508e7
scook5570 <cooksm@merrimack.edu>
2024-04-15 12:07:50 -0400

    Added shell sort call, sorted/unsorted loops, and terminal printouts

--- FinalContent/Main.java
+++ FinalContent/Main.java
@@ -5,58 +5,78 @@ import FinalContent.sortAlgs.*;
 
 public class Main {
     public static void main(String args[]) {
-        int[] arraySizes = { 10, 100 };
+        int[] arraySizes = { 10, 100, 1000, 10000, 50000, 100000, 500000 };
         String[] sortTypes = { "Selection", "Bubble", "Insertion", "Merge", "Quick", "Heap", "Shell", "Radix" };
         RandomArrayGenerator randomArray = new RandomArrayGenerator();
-
+        
         for (int size : arraySizes) {
             int[] intArray = randomArray.generateRandomIntArray(size, 0, size);
             double[] doubleArray = randomArray.generateRandomDoubleArray(size, 0, size);
             String[] stringArray = randomArray.generateRandomStringArray(size);
 
+            int[] intBefore = intArray;
+            double[] doubleBefore = doubleArray;
+            String[] stringBefore = stringArray;
+
             Object[] arrays = { intArray, doubleArray, stringArray };
 
-            for (Object array : arrays) {
-                for (String item : sortTypes) {
-                    long start = System.nanoTime();
-                    switch (item) {
-                        case "Selection":
-                            break;
-                        case "Bubble":
-                            break;
-                        case "Insertion":
-                            break;
-                        case "Merge":
-                            break;
-                        case "Quick":
-                            break;
-                        case "Heap":
-                            break;
-                        case "Shell":
-                            break;
-                        case "Radix":
-                            if (array instanceof int[]) {
-                                Radix.radixSort((int[]) array);
-                                for (int number : (int[])array) {
-                                    System.out.println(number + " ");
+            for (Object array : arrays) { // test each array type
+                for (String item : sortTypes) { // test each sorting type
+                    for (int i = 0; i < 2; i++) { // loop through each sorting type again with sorted array
+                        long start = System.nanoTime();
+
+                        switch (item) {
+                            case "Selection":
+                                break;
+                            case "Bubble":
+                                break;
+                            case "Insertion":
+                                break;
+                            case "Merge":
+                                break;
+                            case "Quick":
+                                break;
+                            case "Heap":
+                                break;
+                            case "Shell":
+                                if (array instanceof int[]) {
+                                    Shell.shellSort((int[]) array);
+                                } else if (array instanceof double[]) {
+                                    Shell.shellSort((double[]) array);
+                                } else if (array instanceof String[]) {
+                                    Shell.shellSort((String[]) array);
                                 }
-                            } else if (array instanceof double[]) {
-                                Radix.radixSort((double[]) array); // broken :(
-                                for (double number : (double[])array) { 
-                                    System.out.println(number + " ");
+                                break;
+                            case "Radix":
+                                if (array instanceof int[]) {
+                                    Radix.radixSort((int[]) array);
+                                } else if (array instanceof double[]) {
+                                    Radix.radixSort((double[]) array); 
+                                } else if (array instanceof String[]) {
+                                    Radix.radixSort((String[]) array);
                                 }
-                            } else if (array instanceof String[]) {
-                                Radix.radixSort((String[]) array); // broken fix it later
-                                for (String string : (String[])array) {
-                                    System.out.println(string + " ");
-                                }
-                            }
-                            break;
-                        default:
-                            System.out.println("Error.");
+                                break;
+                            default:
+                                System.out.println("Error.");
+                        }
+                        String unsortSort;
+                        if (i == 0) {
+                            unsortSort = "Unsorted";
+                        }  else {
+                            unsortSort = "Sorted";
+                        }
+                            System.out.format("%9s, %6d, %19s], %8s, %10d \n", item, size, array.getClass().getName(), unsortSort, (System.nanoTime() - start));
+                            // System.out.println(item + " [size " + size +  " / " + array.getClass().getName() + " / unsorted]: " + (System.nanoTime() - start));
                     }
-                    System.out.println(System.nanoTime() - start);
-                }
+
+                    if (array instanceof int[]) {
+                        array = intBefore;
+                    } else if (array instanceof double[]) {
+                        array = doubleBefore; 
+                    } else if (array instanceof String[]) {
+                        array = stringBefore;
+                    }
+                } 
             }
         }
     }
