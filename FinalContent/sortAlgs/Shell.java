901fd274e90a67f24dfefddb65ab544b2b122696
scook5570 <cooksm@merrimack.edu>
2024-04-15 12:08:13 -0400

    created shell sort for all array types

--- FinalContent/sortAlgs/Shell.java
+++ FinalContent/sortAlgs/Shell.java
@@ -1,5 +1,48 @@
 package FinalContent.sortAlgs;
 
 public class Shell {
-    
+    public static void shellSort(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int temp = arr[i];
+
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void shellSort(double[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                double temp = arr[i];
+
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void shellSort(String[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                String temp = arr[i];
+
+                int j;
+                for (j = i; j >= gap && arr[j - gap].toLowerCase().compareTo(temp.toLowerCase()) > 0; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+
+                arr[j] = temp;
+            }
+        }
+    }
 }
