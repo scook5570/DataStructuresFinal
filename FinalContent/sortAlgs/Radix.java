0594d412d59f5959ff19986a164696e8948dd138
scook5570 <cooksm@merrimack.edu>
2024-04-15 12:08:00 -0400

    added string sort functionality

--- FinalContent/sortAlgs/Radix.java
+++ FinalContent/sortAlgs/Radix.java
@@ -24,7 +24,7 @@ public class Radix {
     public static String getMax(String[] arr) {
         String max = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i].compareTo(max) < 0) {
+            if (arr[i].length() > max.length()) {
                 max = arr[i];
             }
         }
@@ -84,7 +84,46 @@ public class Radix {
     }
 
     public static void countNumbers(String[] arr, int place) {
-        // fill this in later
+        // place starts at 9 then 8...
+        String[] output = new String[arr.length];
+        int[] tally = new int[36]; // capital letters do not take priority: 0 to 9 and a to z
+
+        for (int i = 0; i < tally.length; i++) {
+            tally[i] = 0;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null && arr[i].length() > place) {
+                tally[findVal(arr[i].charAt(place))]++;
+            } else {tally[0]++;}
+        }
+
+        for (int i = 1; i < 36; i++) {
+            tally[i] += tally[i - 1];
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] != null && arr[i].length() > place) {
+                output[tally[findVal(arr[i].charAt(place))] - 1] = arr[i];
+                tally[findVal(arr[i].charAt(place))]--;
+            } else {
+                output[tally[0] - 1] = arr[i];
+                tally[0]--;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static int findVal(char letter) {
+        String lettersAndNumbers = "0123456789abcdefghijklmnopqrstuvwxyz";
+        int index = 0;
+        while (Character.toLowerCase(letter) != lettersAndNumbers.charAt(index)) {
+            index++;
+        }
+        return index;
     }
     
     public static void radixSort(int[] arr) {
@@ -105,8 +144,8 @@ public class Radix {
 
     public static void radixSort(String[] arr) {
         String max = getMax(arr);
-    
-        for (int place = 1; place > 0; place++ ) { // figure this out...
+
+        for (int place = max.length() - 1; place >= 0; place--) {
             countNumbers(arr, place);
         }
     }
