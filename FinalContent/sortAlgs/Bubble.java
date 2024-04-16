3bafb2b89e164cd9b86d2aa46a43dd68134fae07
eleute6 <leutee@merrimack.edu>
2024-04-15 20:27:48 -0400

    added Bubble class, some more formatting things

--- FinalContent/sortAlgs/Bubble.java
+++ FinalContent/sortAlgs/Bubble.java
@@ -1,30 +1,33 @@
 package FinalContent.sortAlgs;
 
 public class Bubble {
-    void bubbleSort(int arr[]) { 
+    public void bubbleSort(int arr[]) { 
         int n = arr.length; 
-        for (int i = 0; i < n - 1; i++) //Each Round, if n elements then n rounds
-            for (int j = 0; j < n - i - 1; j++) // Compare adjacent elements
-                if (arr[j] > arr[j + 1]) { //elements are in the wrong order
-                    // swap temp and arr[i] 
-                    int temp = arr[j]; 
-                    arr[j] = arr[j + 1]; 
-                    arr[j + 1] = temp; 
-                } 
+        for (int i = 0; i < n - 1; i++) 
+        // compares adjacent elements, swaps if in the wrong order  
+        for (int j = 0; j < n - i - 1; j++) {
+                
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j]; 
+                arr[j] = arr[j + 1]; 
+                arr[j + 1] = temp; 
+            } 
+        }
     } 
   
     // Prints the array 
-    void printArray(int arr[]) { 
+    public void printArray(int arr[]) { 
         int n = arr.length; 
         for (int i = 0; i < n; ++i) 
             System.out.print(arr[i] + " "); 
         System.out.println(); 
     } 
   
-    // Driver method to test above 
+ // temporary main to test; will remove later
     public static void main(String args[]) { 
         Bubble ob = new Bubble(); 
-        int arr[] = { 54, 23, 65, 12, 32, 17, 49, 61 }; //example elements in unsorted order
+        // temporary arr to sort
+        int arr[] = { 54, 23, 65, 12, 32, 17, 49, 61 }; 
         ob.bubbleSort(arr); 
         System.out.println("Sorted array"); 
         ob.printArray(arr); 
