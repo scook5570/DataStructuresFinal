197fc9e6ea6ca67416b44c2a05a7f16fafda2ecd
scook5570 <cooksm@merrimack.edu>
2024-04-29 23:51:15 -0400

    added comments

--- FinalContent/DataGeneration/RandomArrayGenerator.java
+++ FinalContent/DataGeneration/RandomArrayGenerator.java
@@ -2,61 +2,69 @@ package FinalContent.DataGeneration;
 
 import java.util.*;
 
-// hello test commit
-
 public class RandomArrayGenerator {
-    static Random random;
+    static Random random; // Static variable to generate random numbers
 
+    // Method to generate an array of random integers
     public int[] generateRandomIntArray(int size, int min, int max, int seed) {
-        int[] arr = new int[size];
-        random = new Random(seed);
+        int[] arr = new int[size]; // Create an array of integers with the specified size
+        random = new Random(seed); // Initialize the random number generator with the given seed
         for (int i = 0; i < size; i++) {
+            // Generate random integer within the specified range and assign it to the array
             arr[i] = random.nextInt(max - min + 1) + min;
         }
-        return arr;
+        return arr; // Return the generated array
     }
 
+    // Method to generate an array of random doubles
     public double[] generateRandomDoubleArray(int size, int min, int max, int seed) {
-        double[] arr = new double[size];
-        random = new Random(seed);
+        double[] arr = new double[size]; // Create an array of doubles with the specified size
+        random = new Random(seed); // Initialize the random number generator with the given seed
         for (int i = 0; i < size; i++) {
+            // Generate random double within the specified range and assign it to the array
             arr[i] = min + (max - min) * random.nextDouble();
         }
-        return arr;
+        return arr; // Return the generated array
     }
 
+    // Method to generate an array of random strings
     public String[] generateRandomStringArray(int size, int seed) {
         String lettersAndNumbers = "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-        String[] arr = new String[size];
-        random = new Random(seed);
+        String[] arr = new String[size]; // Create an array of strings with the specified size
+        random = new Random(seed); // Initialize the random number generator with the given seed
         for (int i = 0; i < arr.length; i++) {
             String temp = "";
+            // Generate a random string of length between 1 and 15 characters
             for (int j = 0; j < random.nextInt(15) + 1; j++) {
+                // Append random characters from the lettersAndNumbers string to form the random string
                 temp = temp.concat(String.valueOf(lettersAndNumbers.charAt(random.nextInt(lettersAndNumbers.length()))));
             }
-            arr[i] = temp;
+            arr[i] = temp; // Assign the generated string to the array
         }
-        return arr;
+        return arr; // Return the generated array
     }
 
+    // Method to print an array of integers
     public void printArr(int[] arr) {
         for (int item : arr) {
-            System.out.print(item + " ");
+            System.out.print(item + " "); // Print each element of the array followed by a space
         }
-        System.out.println();
+        System.out.println(); // Move to the next line after printing the array
     }
 
+    // Method to print an array of doubles
     public void printArr(double[] arr) {
         for (double item : arr) {
-            System.out.print(item + " ");
+            System.out.print(item + " "); // Print each element of the array followed by a space
         }
-        System.out.println();
+        System.out.println(); // Move to the next line after printing the array
     }
 
+    // Method to print an array of strings
     public void printArr(String[] arr) {
         for (String item : arr) {
-            System.out.print(item + " ");
+            System.out.print(item + " "); // Print each element of the array followed by a space
         }
-        System.out.println();
+        System.out.println(); // Move to the next line after printing the array
     }
 }
