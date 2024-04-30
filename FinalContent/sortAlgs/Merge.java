e9c15905a6765458638c619bb6ac665f976832c1
Webera27 <167246056+Webera27@users.noreply.github.com>
2024-04-30 14:10:06 -0400

    finalized merge

--- FinalContent/sortAlgs/Merge.java
+++ FinalContent/sortAlgs/Merge.java
@@ -1,5 +1,124 @@
 package FinalContent.sortAlgs;
 
-public class Merge {
-    
+public class FinalMerge {
+
+    void merge(int arr[], int l, int r, int m) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    void merge(double arr[], int l, int r, int m) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        double L[] = new double[n1];
+        double R[] = new double[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    void sort(double arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    void merge(String arr[], int l, int r, int m) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        String L[] = new String[n1];
+        String R[] = new String[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].compareTo(R[j]) <= 0) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(String arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
 }
+
