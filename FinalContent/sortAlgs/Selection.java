838804c5c4de5258a5b6a4ac4818e0de0270de74
Webera27 <167246056+Webera27@users.noreply.github.com>
2024-04-30 14:11:48 -0400

    finalized selection sort

--- FinalContent/sortAlgs/Selection.java
+++ FinalContent/sortAlgs/Selection.java
@@ -1,55 +1,54 @@
 package FinalContent.sortAlgs;
  
-   import java.io.*;
-
+  
 public class FinalSelection {
     
-        void sort(int arr[])
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
         {
-            int n = arr.length;
-            for (int i = 0; i < n-1; i++)
-            {
-                int minIndex = i;
-                for (int j = i+1; j < n; j++)
-                    if (arr[j] < arr[minIndex])
-                        minIndex = j;
+            int minIndex = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
 
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
-            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
+    }
 
-        void sort(double arr[])
+    void sort(double arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
         {
-            int n = arr.length;
-            for (int i = 0; i < n-1; i++)
-            {
-                int minIndex = i;
-                for (int j = i+1; j < n; j++)
-                    if (arr[j] < arr[minIndex])
-                        minIndex = j;
+            int minIndex = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
 
-                double temp = arr[minIndex];
-                //change these 
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
-            }
+            double temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
-        void sort(String arr[])
+    }
+    
+    void sort(String arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
         {
-            int n = arr.length;
-            for (int i = 0; i < n-1; i++)
-            {
-                int minIndex = i;
-                for (int j = i+1; j < n; j++)
-                    if (arr[j] < arr[minIndex])
-                        minIndex = j;
+            int minIndex = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j].compareTo(arr[minIndex]) < 0)
+                    minIndex = j;
 
-                String temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
-            }
+            String temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
+    }
 
 }
